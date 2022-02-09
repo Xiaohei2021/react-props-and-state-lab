8a4756d1c88dfbd1e5dfe5b4ff046d5e92f5276b
Leon <hualiang.tian@gmail.com>
2022-02-09 16:06:55 -0500

    commit tree ccf37aa3806e9744305ce7bd892af218908ef996

--- src/components/App.js
+++ src/components/App.js
@@ -15,22 +15,106 @@ class App extends React.Component {
     }
   }
 
+  handleFindPets = () =>{
+    switch(this.state.filters.type){
+
+      case 'cat':
+        fetch('/api/pets?type=cat')
+        .then(r => r.json())
+        .then(data=>{
+          this.setState({
+            pets: data
+          })
+        })
+        break;
+
+      case 'dog':
+        fetch('/api/pets?type=dog')
+        .then(r => r.json())
+        .then(data=>{
+          this.setState({
+            pets: data
+          })
+        })
+
+        break;
+
+      case 'micropig':
+        fetch('/api/pets?type=micropig')
+        .then(r => r.json())
+        .then(data=>{
+          this.setState({
+            pets: data
+          })
+        })
+        
+        break;
+      
+      default:
+        fetch('/api/pets')
+        .then(r => r.json())
+        .then(data=>{
+          this.setState({
+            pets: data
+          })
+        })
+        break;
+    }
+  }
+
+  handleAdoptPet = (furFriendId) => {
+    
+    const newFam = this.state.pets.map(pet => {return pet.id === furFriendId? {...pet, isAdopted:true} : pet} )
+
+    this.setState(
+      {
+        pets:newFam      
+      }
+    )
+
+    // this.setState(
+    //   {
+    //     pets:{...this.state.pets.filter(pet => pet.id === id)},  isAdopted: true      
+    //   }
+    // )
+  }
+
+  handleChangetype = (e) => {
+    this.setState({
+      filters: {...this.state.filters, type: e.target.value}
+    }, ()=> console.log(this.state.filters.type))
+  } 
+
+  // handleChangetype = ({target:{value}}) => {
+
+  //   this.setState({
+  //     filters: {...this.state.filters, type: value}
+  //   }, ()=> console.log(this.state.filters.type))
+  // } 
+
+
   render() {
     return (
       <div className="ui container">
+
         <header>
           <h1 className="ui dividing header">React Animal Shelter</h1>
         </header>
+
         <div className="ui container">
+
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onFindPetsClick={this.handleFindPets} onChangeType={this.handleChangetype}  />
             </div>
+
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.handleAdoptPet}/>
             </div>
           </div>
+
         </div>
+
       </div>
     )
   }
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -4,7 +4,13 @@ import Pet from './Pet'
 
 class PetBrowser extends React.Component {
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return <div className="ui cards">
+              PET COMPONENT SHOULD GO HERE
+              {/* {console.log(this.props.pets)} */}
+              {this.props.pets.map(
+                  furFriend => <Pet key={furFriend.id} pet={furFriend} onAdoptPet={this.props.onAdoptPet} />
+                )}
+            </div>
   }
 }
 
